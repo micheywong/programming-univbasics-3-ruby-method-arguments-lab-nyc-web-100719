99cbbfae1cae01ea3835b2762f463171ef35611f
Michelle Wong <michellekwong2@gmail.com>
2019-09-21 18:41:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 name = "Josh"
-name = 
+name = "Dan"
 def introduction (name)
   puts "Hi, my name is #{name}"
 end
