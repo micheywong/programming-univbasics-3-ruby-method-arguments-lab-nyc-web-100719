04063a8275a99e571b3e10cca44634a5c3def9f4
Michelle Wong <michellekwong2@gmail.com>
2019-09-21 18:43:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,9 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction_with_language("Josh")
+introduction("Dan")
+
 def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
